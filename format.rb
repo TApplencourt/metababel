cb89fa86160d66af141f27bac68bdf1523cbcbc9
Thomas Applencourt <tapplencourt@anl.gov>
2022-10-25 23:43:20 +0000

    Fix indent

--- format.rb
+++ format.rb
@@ -1,41 +1,40 @@
 require 'open3'
 
 def format_erb(str_)
-r = /<%\s+(.*?)\s*%>/
+  r = /<%\s+(.*?)\s*%>/
 
-d_sub = {}
-i = 0
+  d_sub = {}
+  i = 0
 
-# Handle <%=
-str_.gsub!(/<%=\s+(.*?)\s*%>/){ |m|
-    prefix = "_"*(m.size - i.to_s.size - 1)
-    new_str =  "#{prefix}#{i}_" # Avoid 10 matching 1
-    i=i+1
+  # Handle <%=
+  str_.gsub!(/<%=\s+(.*?)\s*%>/) do |m|
+    prefix = '_' * (m.size - i.to_s.size - 1)
+    new_str = "#{prefix}#{i}_" # Avoid 10 matching 1
+    i += 1
     d_sub[new_str] = m
     new_str
-}
+  end
 
-# Handle <%
-str_.gsub!(/<%#?\s+(.*?)\s*%>/){ |m|
-    new_str =  "//___#{i}___"
-    i=i+1
+  # Handle <%
+  str_.gsub!(/<%#?\s+(.*?)\s*%>/) do |m|
+    new_str = "//___#{i}___"
+    i += 1
     d_sub[new_str] = m
     new_str
-}
+  end
 
+  str_, = Open3.capture2('clang-format', stdin_data: str_)
 
-str_, _ = Open3.capture2("clang-format", :stdin_data=>str_)
+  d_sub.each do |k, v|
+    str_.gsub!(/#{k}/, v)
+  end
 
-d_sub.each { |k,v|
-  str_.gsub!(/#{k}/,v)
-}
-
-str_
+  str_
 end
 
-ARGV.each { |f|
-  
-  str_ = File.read(ARGV[0])
+ARGV.each do |f|
+  puts "Formating #{f}"
+  str_ = File.read(f)
   str_ = format_erb(str_)
-  File.write(ARGV[0], str_)
-}
+  File.write(f, str_)
+end
