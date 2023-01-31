e237a626c536529b21eeb83b92b846ee540ad059
Thomas Applencourt <apl@anl.gov>
2023-01-31 21:28:29 +0000

    Better param API

--- example/SOURCE.xprof/my_callbacks.c
+++ example/SOURCE.xprof/my_callbacks.c
@@ -3,7 +3,10 @@
 #include <stdio.h>
 
 void btx_push_usr_messages(struct common_data_s *common_data) {
-    printf("ENV: %s\n", common_data->usr_params->display );
+    params_t *params = (params_t*) malloc(sizeof(params_t));
+    btx_read_params(common_data, params);
+    printf("PARAM DISPLAY: %s\n", params->display);
+    free(params);
     btx_push_message_lttng_ust_ze_profiling_event_profiling_results(common_data, 10, 10, 20);
     btx_push_message_lttng_ust_ze_profiling_event_profiling_results(common_data, 11, 10, 30);
     btx_push_message_lttng_ust_ze_profiling_event_profiling_results(common_data, 12, 10, 40);
