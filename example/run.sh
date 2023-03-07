ae120ea1760146fbbe20ad4203046b3184f12e65
Thomas Applencourt <tapplencourt@anl.gov>
2023-03-07 16:29:47 +0000

    Step closer to a gemm

--- example/run.sh
+++ example/run.sh
@@ -1,13 +1,13 @@
-#!/bin/bash -x
+#!/bin/bash -xe
 
-ruby ../main.rb -d 1.stream_classes.yaml -t SOURCE -p metababel1 --params 1.params.yaml 
+ruby -I../lib ../bin/metababel -d 1.stream_classes.yaml -t SOURCE -p metababel1 --params 1.params.yaml 
 make -C SOURCE.xprof/
 babeltrace2 --plugin-path=SOURCE.xprof --component=source.metababel1.xprof --params="display=tests2323" --component=sink.text.details 
 
-ruby ../main.rb -u 1.stream_classes.yaml -t SINK -p metababel2
+ruby -I../lib ../bin/metababel -u 1.stream_classes.yaml -t SINK -p metababel2
 make -C SINK.xprof
 babeltrace2  --plugin-path=SOURCE.xprof:SINK.xprof  --component=source.metababel1.xprof --component=sink.metababel2.xprof
 
-ruby ../main.rb -u 1.stream_classes.yaml -d 2.stream_classes.yaml -t FILTER -p metababel3
+ruby -I../lib ../bin/metababel -u 1.stream_classes.yaml -d 2.stream_classes.yaml -t FILTER -p metababel3
 make -C FILTER.xprof
 babeltrace2  --plugin-path=SOURCE.xprof:FILTER.xprof  --component=source.metababel1.xprof --component=filter.metababel3.xprof
---
+++ lib/metababel.rb
@@ -0,0 +1,3 @@
+require 'metababel/bt2_generator_utils'
+require 'metababel/bt2_stream_classes_generator'
+require 'metababel/bt2_values_generator'
