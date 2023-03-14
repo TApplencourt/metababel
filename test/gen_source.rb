059cf12aaf30a4e823abfb0a5cf4f8095a1f0302
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2023-03-14 16:47:37 +0000

    tests generation

---
+++ test/gen_source.rb
@@ -0,0 +1,114 @@
+require 'optparse'
+require 'yaml'
+require 'erb'
+
+
+REGEXT_PRETTY = %r{
+ =\s            # We are interested to the right of the equal sign
+ (
+    ""|         # Empty string
+    ".*?[^\\]"| # String who can contain space and quoted string
+    [^\s,]+     # Anything except space and comma
+ )
+}x
+
+SOURCE_TEMPLATE =  <<-TEXT
+/* Code generaed by source_callbacks_generator.rb */
+
+#include "component.h"
+#include "create.h"
+
+void btx_push_usr_messages(struct common_data_s *common_data) {
+
+    <%- data.each do | entry | -%>
+    <%- entry.fetch(:times,1).times do -%>
+    btx_push_message_<%= entry[:name].gsub(":","_") %>(common_data,<%=  entry[:field_values].join(",") %>);
+    <%- end -%>
+    <%- end -%>
+}
+
+TEXT
+
+def parse_log(input_path)
+  data_list = []
+  File.open(input_path, "r") do |f|
+    f.each_line do |line|
+
+      # Line format support checks
+      match = line.match(REGEXT_PRETTY)
+      raise "Unsupported format for '#{line}'." unless match
+
+      i = line.index('{')
+      head, _, tail = line.partition(": {")
+      
+      data = {}
+      data[:name]=head.gsub(/[^0-9A-Za-z\-]/, '_') # Should reuse metababel mangling
+      data[:field_values] = tail.scan(REGEXT_PRETTY).flatten
+      data_list.append(data)
+    end
+  end
+  data_list
+end
+
+def parse_yaml(input_path)
+  data = YAML.load_file(input_path)
+  data.each do |item| 
+    item[:field_values] = item[:field_values].map(&:inspect)
+  end 
+end 
+
+def render_and_save(data, output_path) 
+  renderer = ERB.new(SOURCE_TEMPLATE, nil, '-')
+  output = renderer.result(binding)
+  File.write(output_path, output, mode: "w")
+end
+
+DOCS = <<-DOCS
+  Usage: example.rb [options]  
+  
+  Example: 
+    ruby example.rb -t yaml -i  ./3.interval_instances.yaml -o ./interval_callbacks.c
+    ruby example.rb -t log -f EE -i babeltrace.log -o  ./entry_exit_callbacks.c
+DOCS
+
+options = {}
+
+OptionParser.new do |opts|
+  opts.banner = 'Usage: example.rb [options]'
+
+  opts.on("-h", "--help", "Prints this help") do
+      puts DOCS
+      exit
+  end
+
+  opts.on('-t', '--type TYPE', "[Mandatory] 'yaml|log'.") do |p|
+    options[:input_type] = p
+  end
+
+  opts.on('-i', '--input PATH', '[Mandatory] Path to the bt2 yaml file.') do |p|
+    options[:input_path] = p
+  end
+
+  opts.on('-o', '--output PATH', '[Optional] Path to the bt2 SOURCE file.') do |p|
+    options[:output_path] = p
+  end
+
+end.parse!
+
+raise OptionParser::MissingArgument if options[:input_type].nil?
+raise OptionParser::MissingArgument if options[:input_path].nil?
+raise OptionParser::MissingArgument if options[:output_path].nil?
+
+
+case options[:input_type]
+when "yaml"
+  render_and_save(
+    parse_yaml(options[:input_path]), 
+    options[:output_path])
+when "log"
+  render_and_save( 
+    parse_log(options[:input_path]), 
+    options[:output_path])
+else
+  "Error: unknown argument for --type (-t) #{options[:input_type]}"
+end
---
+++ test/gen_yaml_and_log.rb
@@ -0,0 +1,129 @@
+require "erb"
+require "securerandom"
+require 'yaml'
+
+# TODO: Need to know how to set the seed.
+# Kernel.srand(1)
+
+SAMPLES = 2
+CFIELDS = 2
+PFIELDS = 2
+LENGTH = 20
+
+EVENT_CLASS_NAME_GEN = lambda do 
+    @counter = @counter + 1
+    "test:semicolon_#{@counter}"    
+end 
+STRING_VALUE_GEN = lambda { SecureRandom.base64(LENGTH) }
+INTEGER_VALUE_GEN = lambda { SecureRandom.random_number(LENGTH) }
+BOOLEAN_VALUE_GEN = lambda { [true,false].sample }
+
+FIELD_TYPE_TO_VALUE = {
+    "string" => STRING_VALUE_GEN,
+    "integer_unsigned" => INTEGER_VALUE_GEN,
+    "bool" => BOOLEAN_VALUE_GEN
+}
+
+# CFIELDS_VALUE_GENS = [ STRING_VALUE_GEN ]
+# PFIELDS_VALUE_GENS = [ INTEGER_VALUE_GEN ]
+
+def gen_integer_field
+    @counter = 0
+    def gen_integer_field_wrapped
+        @counter = @counter + 1 
+        {
+            :name => "if_#{@counter}",
+            :field_class => {
+                :type => 'integer_unsigned',
+                :field_value_range => 32,
+                :cast_type =>  'int'
+            }
+        }
+    end
+end 
+
+def gen_string_field
+    @counter = 0
+    def gen_string_field_wrapped
+        @counter = @counter + 1
+        {
+            :name => "sf_#{@counter}",
+            :field_class => {
+                :type => 'string',
+                :cast_type => 'const char*'
+            }
+        }
+    end
+end 
+
+def gen_bool_field
+    @counter  = 0
+    def gen_bool_field_wrapped 
+        @counter = @counter + 1
+        {
+            :name => "bf_#{@counter}",
+            :field_class => {
+                :type => 'bool',
+                :cast_type => 'bt_bool'
+            }
+        }
+    end
+end
+
+CFIELDS_DEF_GENS = [ gen_bool_field, gen_string_field, gen_integer_field ]
+PFIELDS_DEF_GENS = [ gen_bool_field, gen_string_field, gen_integer_field ]
+
+def gen_event_class
+    {
+        :name => EVENT_CLASS_NAME_GEN.call,
+        :payload_field_class => {
+            :type => 'structure',
+            :members => PFIELDS.times.map { send PFIELDS_DEF_GENS.sample }
+        }
+    }
+end
+
+
+STREAM_CLASSES = {
+    :stream_classes => [
+        {
+            :name => 'sc1',
+            :event_common_context_field_class => {
+                :type => "structure",
+                :members => CFIELDS.times.map { send CFIELDS_DEF_GENS.sample }
+            },
+            :event_classes => PFIELDS.times.map { gen_event_class }
+        }
+    ]
+}
+
+File.open("test_stream_classes.yml", "w") do |file| 
+    file.write(STREAM_CLASSES.to_yaml)
+end
+
+common_members = STREAM_CLASSES[:stream_classes][0][:event_common_context_field_class][:members]
+event_classes = STREAM_CLASSES[:stream_classes][0][:event_classes]
+
+data = []
+event_classes.map do | ec |
+    ec_data = {}
+    ec_data[:name] = ec[:name]
+    ec_data[:common] = common_members.map do |member|
+        "#{member[:name]} = #{ FIELD_TYPE_TO_VALUE[member[:field_class][:type]].call.inspect }"
+    end.join(", ")
+
+    ec_data[:payload] = ec[:payload_field_class][:members].map do |member| 
+        "#{member[:name]} = #{ FIELD_TYPE_TO_VALUE[member[:field_class][:type]].call.inspect }" 
+    end.join(", ")
+    data.append(ec_data)
+end
+
+METABABEL_LOG_TEMPLATE =  <<-TEXT
+<%- data.each do | entry | -%>
+<%= entry[:name] %>: { <%= entry[:common] %> }, { <%= entry[:payload] %> } 
+<%- end -%>
+TEXT
+
+renderer = ERB.new(METABABEL_LOG_TEMPLATE, nil, '-')
+output = renderer.result(binding)
+File.write("test_babeltrace_output.txt", output, mode: "w")
