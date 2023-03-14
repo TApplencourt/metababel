65458a5e2f6ddb7b3958f72506ce23e81a2313fd
Thomas Applencourt <tapplencourt@anl.gov>
2023-03-14 16:23:54 +0000

    Fix example

--- test/SOURCE.metababel/callbacks.c
+++ test/SOURCE.metababel/callbacks.c
@@ -6,7 +6,7 @@
 void btx_initialize_usr_data(common_data_t *common_data, void **usr_data) {
 }
 
-btx_source_status_t btx_push_usr_messages(struct common_data_s *common_data) {
+btx_source_status_t btx_push_usr_messages(struct common_data_s *common_data, void *usr_data) {
     btx_push_message_test0(common_data, -10, INT32_MAX+1U, UINT32_MAX);
     btx_push_message_test1_with_colon(common_data, -11, 0, UINT32_MAX+1U, "const char *");
     return BTX_SOURCE_END;
