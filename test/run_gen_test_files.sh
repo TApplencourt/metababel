baa6303d6d7ab1e6180f3128d465d63221b8423e
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2023-03-14 19:57:22 +0000

    update

--- test/run_gen_test_files.sh
+++ test/run_gen_test_files.sh
@@ -5,4 +5,4 @@ set -e
 # Generates 'test_babeltrace_output.txt' and 'test_stream_classes.yaml'
 ruby gen_yaml_and_log.rb
 ruby gen_source.rb -t log -i test_babeltrace_output.txt -o test_callbacks_1.c
-ruby gen_source.rb -t yaml -i 3.interval_instances.yaml -o test_callbacks_2.c
+ruby gen_source.rb -t yaml -i ../display/3.interval_instances.yaml -o test_callbacks_2.c
