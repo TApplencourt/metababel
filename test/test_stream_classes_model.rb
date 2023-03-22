fcf53b72b7a2da3132a9e3854ffbd9d5b9030bf8
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2023-03-22 15:43:08 -0500

    Test files generation (#15)
    * tests generation
    
    * tests generation
    
    * update
    
    * update
    
    * btx model and instances generation
    
    * model and log generation, basic test
    
    * basic test cases
    
    * first tests
    
    * fix readme
    
    * Stil organizing tests
    
    * basic tests finished
    
    * cleaning up
    
    * sanitize large integers
    
    * int32 test
    
    * update

---
+++ test/test_stream_classes_model.rb
@@ -0,0 +1,86 @@
+require 'base_test'
+
+class TestSourceNoCommonField < Test::Unit::TestCase
+    include TestSourceBase
+    extend VariableAccessor
+    include VariableClassAccessor
+
+    def self.startup
+        @btx_variables = {
+            btx_model_path: './test/test_cases_stream_classes_model/1.btx_model.yaml',
+            btx_target_log_path: './test/test_cases_stream_classes_model/1.btx_log.txt',
+            btx_component_type: 'SOURCE',
+            btx_component_name: 'source',
+            btx_pluggin_name: 'metababel_tests',
+            btx_component_path: './test/SOURCE.metababel_test'
+        }
+    end
+end
+
+class TestSourceNoPayloadField< Test::Unit::TestCase
+    include TestSourceBase
+    extend VariableAccessor
+    include VariableClassAccessor
+
+    def self.startup
+        @btx_variables = {
+            btx_model_path: './test/test_cases_stream_classes_model/2.btx_model.yaml',
+            btx_target_log_path: './test/test_cases_stream_classes_model/2.btx_log.txt',
+            btx_component_type: 'SOURCE',
+            btx_component_name: 'source',
+            btx_pluggin_name: 'metababel_tests',
+            btx_component_path: './test/SOURCE.metababel_test'
+        }
+    end
+end
+
+class TestSourceNoCommonNoPayloadFields < Test::Unit::TestCase
+    include TestSourceBase
+    extend VariableAccessor
+    include VariableClassAccessor
+
+    def self.startup
+        @btx_variables = {
+            btx_model_path: './test/test_cases_stream_classes_model/3.btx_model.yaml',
+            btx_target_log_path: './test/test_cases_stream_classes_model/3.btx_log.txt',
+            btx_component_type: 'SOURCE',
+            btx_component_name: 'source',
+            btx_pluggin_name: 'metababel_tests',
+            btx_component_path: './test/SOURCE.metababel_test'
+        }
+    end
+end
+
+class TestSourceCommonPayloadFields < Test::Unit::TestCase
+    include TestSourceBase
+    extend VariableAccessor
+    include VariableClassAccessor
+
+    def self.startup
+        @btx_variables = {
+            btx_model_path: './test/test_cases_stream_classes_model/4.btx_model.yaml',
+            btx_target_log_path: './test/test_cases_stream_classes_model/4.btx_log.txt',
+            btx_component_type: 'SOURCE',
+            btx_component_name: 'source',
+            btx_pluggin_name: 'metababel_tests',
+            btx_component_path: './test/SOURCE.metababel_test'
+        }
+    end
+end
+
+class TestSourceHundredFields < Test::Unit::TestCase
+    include TestSourceBase
+    extend VariableAccessor
+    include VariableClassAccessor
+
+    def self.startup
+        @btx_variables = {
+            btx_model_path: './test/test_cases_stream_classes_model/5.btx_model.yaml',
+            btx_target_log_path: './test/test_cases_stream_classes_model/5.btx_log.txt',
+            btx_component_type: 'SOURCE',
+            btx_component_name: 'source',
+            btx_pluggin_name: 'metababel_tests',
+            btx_component_path: './test/SOURCE.metababel_test'
+        }
+    end
+end
---
+++ test/test_type_uint.rb
@@ -0,0 +1,59 @@
+require 'base_test'
+
+class TestSourceTypeUInt64ZeroValue < Test::Unit::TestCase
+    include TestSourceBase
+    extend VariableAccessor
+    include VariableClassAccessor
+
+    def self.startup
+        @btx_variables = {
+            btx_model_path: './test/test_cases_type_uint/1.btx_model.yaml',
+            btx_target_log_path: './test/test_cases_type_uint/1.1.btx_log.txt',
+            btx_component_type: 'SOURCE',
+            btx_component_name: 'source',
+            btx_pluggin_name: 'metababel_tests',
+            btx_component_path: './test/SOURCE.metababel_test',
+        }
+    end
+end
+
+class TestSourceTypeUInt64MaxValue < Test::Unit::TestCase
+    include TestSourceBase
+    extend VariableAccessor
+    include VariableClassAccessor
+
+    def self.startup
+        @btx_variables = {
+            btx_model_path: './test/test_cases_type_uint/1.btx_model.yaml',
+            btx_target_log_path: './test/test_cases_type_uint/1.2.btx_log.txt',
+            btx_component_type: 'SOURCE',
+            btx_component_name: 'source',
+            btx_pluggin_name: 'metababel_tests',
+            btx_component_path: './test/SOURCE.metababel_test'
+        }
+    end
+
+    # We need to use the btx_model.yaml in the generation to apply 
+    # the UINT64 or INT64 macro accordingly for long integers.
+    def subtest_generate_source_callbacks
+        `ruby ./test/gen_source.rb -i #{btx_variables[:btx_target_log_path]} -y #{btx_variables[:btx_model_path]} -o #{btx_variables[:btx_component_path]}/callbacks.c`
+        assert($?.success?)
+    end
+end
+
+class TestSourceTypeUInt32MaxValueNoCastType < Test::Unit::TestCase
+    include TestSourceBase
+    extend VariableAccessor
+    include VariableClassAccessor
+
+    def self.startup
+        @btx_variables = {
+            btx_model_path: './test/test_cases_type_uint/2.btx_model.yaml',
+            btx_target_log_path: './test/test_cases_type_uint/2.1.btx_log.txt',
+            btx_component_type: 'SOURCE',
+            btx_component_name: 'source',
+            btx_pluggin_name: 'metababel_tests',
+            btx_component_path: './test/SOURCE.metababel_test'
+        }
+    end
+end
