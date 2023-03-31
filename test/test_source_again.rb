91bad1a7ec3e48f9194abbfd7ab2800742cbe7bf
Thomas Applencourt <apl@anl.gov>
2023-03-31 21:04:47 +0000

    Add source again

---
+++ test/test_source_again.rb
@@ -0,0 +1,24 @@
+require 'base_test'
+
+class TestSourceTypeAgain < Test::Unit::TestCase
+  include TestSourceBase
+  extend VariableAccessor
+  include VariableClassAccessor
+
+  def self.startup
+    @btx_variables = {
+      btx_model_path: './test/cases_source_again/1.btx_model.yaml',
+      btx_target_log_path: './test/cases_source_again/1.btx_log.txt',
+      btx_component_type: 'SOURCE',
+      btx_component_name: 'source',
+      btx_pluggin_name: 'metababel_tests',
+      btx_callbacks_path: './test/cases_source_again/1.callbacks.c',
+      btx_component_path: './test/SOURCE.metababel_test'
+    }
+  end
+
+  def subtest_generate_source_callbacks
+	FileUtils.cp(btx_variables[:btx_callbacks_path],btx_variables[:btx_component_path])
+  end
+
+end
