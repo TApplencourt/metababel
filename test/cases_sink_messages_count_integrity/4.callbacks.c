54a517a9aafb3b81455ebaa9f13160194a8b4250
Thomas Applencourt <tapplencourt@anl.gov>
2023-04-03 22:12:46 +0000

    Use opaque pointer for user

--- test/cases_sink_messages_count_integrity/4.callbacks.c
+++ test/cases_sink_messages_count_integrity/4.callbacks.c
@@ -10,7 +10,7 @@ struct data_s {
 
 typedef struct data_s data_t;
 
-void btx_initialize_usr_data(common_data_t *common_data, void **usr_data)
+void btx_initialize_usr_data(void *btx_handle, void **usr_data)
 {
   data_t *data = malloc(sizeof(data_t *));
   *usr_data = data;
@@ -19,7 +19,7 @@ void btx_initialize_usr_data(common_data_t *common_data, void **usr_data)
   data->event_2_calls_count = 0;
 }
 
-void btx_finalize_usr_data(common_data_t *common_data, void *usr_data)
+void btx_finalize_usr_data(void *btx_handle, void *usr_data)
 {
   data_t *data = (data_t *) usr_data;
 
@@ -30,7 +30,7 @@ void btx_finalize_usr_data(common_data_t *common_data, void *usr_data)
 }
 
 static void event_1_0(
-  common_data_t *common_data, void *usr_data,
+  void *btx_handle, void *usr_data,
   bt_bool cf_1, const char* cf_2, uint64_t cf_3, int64_t cf_4, bt_bool pf_1, const char* pf_2, uint64_t pf_3, int64_t pf_4)
 {
   data_t *data = (data_t *) usr_data;
@@ -38,14 +38,14 @@ static void event_1_0(
 }
 
 static void event_2_0(
-  common_data_t *common_data, void *usr_data,
+  void *btx_handle, void *usr_data,
   bt_bool cf_1, const char* cf_2, uint64_t cf_3, int64_t cf_4, const char* pf_2, bt_bool pf_1, uint64_t pf_3, int64_t pf_4)
 {
   data_t *data = (data_t *) usr_data;
   data->event_2_calls_count += 1;
 }
 
-void btx_register_usr_callbacks(name_to_dispatcher_t** name_to_dispatcher) {
-  btx_register_callbacks_event_1(name_to_dispatcher, &event_2_0);
-  btx_register_callbacks_event_2(name_to_dispatcher, &event_1_0);
+void btx_register_usr_callbacks(void* btx_handle) {
+  btx_register_callbacks_event_1(btx_handle, &event_2_0);
+  btx_register_callbacks_event_2(btx_handle, &event_1_0);
 }
