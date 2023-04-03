b0ed697ef3fa6d5364c9011a0d0d62866c8f0fb0
Thomas Applencourt <tapplencourt@anl.gov>
2023-04-03 14:27:12 +0000

    use correct struct name

--- test/cases_source_again/1.callbacks.c
+++ test/cases_source_again/1.callbacks.c
@@ -6,19 +6,20 @@ struct usr_data_s {
     uint64_t i;
     int count;
 };
+typedef struct usr_data_s usr_data_t;
 
 void btx_initialize_usr_data(common_data_t *common_data, void **usr_data) {
-    struct usr_data_s *data = (struct usr_data_s *)  malloc(sizeof(struct usr_data_s));
+    usr_data_t *data = (usr_data_t *)  malloc(sizeof(usr_data_t));
     *usr_data = data;
     data->i = 0;
     data->count = 4;
 }
 
-btx_source_status_t btx_push_usr_messages(struct common_data_s *common_data, void *usr_data) {
-    struct usr_data_s *data = (struct usr_data_s *) usr_data;
+btx_source_status_t btx_push_usr_messages(common_data_t *common_data, void *usr_data) {
+    usr_data_t *data = (usr_data_t *) usr_data;
     btx_push_message_event_2(common_data, data->i);
     data->i++;
     if (data->i < data->count)
-	return BTX_SOURCE_OK;
+        return BTX_SOURCE_OK;
     return BTX_SOURCE_END;
 }
