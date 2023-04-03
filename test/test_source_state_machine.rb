26ed1782023dc8cd3dc3b1bd88b23b26d4bea521
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2023-04-03 18:59:01 +0000

    cleaning up

--- test/test_source_state_machine.rb
+++ test/test_source_state_machine.rb
@@ -7,12 +7,12 @@ class TestSourceStateMachineAgain < Test::Unit::TestCase
 
   def self.startup
     @btx_source_variables = {
-      btx_model_path: './test/cases_source_again/1.btx_model.yaml',
-      btx_log_path: './test/cases_source_again/1.btx_log.txt',
+      btx_model_path: './test/cases_source_state_machine/1.btx_model.yaml',
+      btx_log_path: './test/cases_source_state_machine/1.btx_log.txt',
       btx_component_type: 'SOURCE',
       btx_component_name: 'source',
       btx_pluggin_name: 'metababel_tests',
-      btx_callbacks_path: './test/cases_source_again/1.callbacks.c',
+      btx_callbacks_path: './test/cases_source_state_machine/1.callbacks.c',
       btx_component_path: './test/SOURCE.metababel_test'
     }
   end
