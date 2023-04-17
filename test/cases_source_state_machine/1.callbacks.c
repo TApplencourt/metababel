d2e0ce852d37879093a0f7e8243d8956a5e1cb37
Thomas Applencourt <tapplencourt@anl.gov>
2023-04-17 21:51:24 +0000

    Minor refactor

--- test/cases_source_state_machine/1.callbacks.c
+++ test/cases_source_state_machine/1.callbacks.c
@@ -10,20 +10,16 @@ typedef struct usr_data_s usr_data_t;
 
 void btx_initialize_usr_data(void *btx_handle, void **usr_data) {
   usr_data_t *data = (usr_data_t *)malloc(sizeof(usr_data_t));
-  *usr_data = data;
   data->i = 0;
   data->count = 4;
+  *usr_data = data;
 }
 
 void btx_push_usr_messages(void *btx_handle, void *usr_data,
                            btx_source_status_t *status) {
   usr_data_t *data = (usr_data_t *)usr_data;
-  btx_push_message_event_2(btx_handle, data->i);
-  data->i++;
-  if (data->i < data->count)
-    *status = BTX_SOURCE_OK;
-  else
-    *status = BTX_SOURCE_END;
+  btx_push_message_event_2(btx_handle, data->i++);
+  *status = (data->i < data->count) ? BTX_SOURCE_OK : BTX_SOURCE_END;
 }
 
 void btx_register_usr_callbacks(void *btx_handle) {
