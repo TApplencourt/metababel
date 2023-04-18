3b0092382717fbb16c105595f1ba49ff61161fba
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2023-04-18 22:28:31 +0000

    test push messages from init and finalize in source and filter

--- test/test_filter_state_machine.rb
+++ test/test_filter_state_machine.rb
@@ -50,3 +50,59 @@ class TestFilterStateMachine < Test::Unit::TestCase
     @btx_connect = [ 'A:C', 'B:C', 'C:D' ]
   end
 end
+
+class TestFilterStateMachinePushMessagesInitialize < Test::Unit::TestCase
+  include GenericTest
+  extend VariableAccessor
+  include VariableClassAccessor
+
+  def self.startup
+    @btx_components = [
+      {
+        btx_component_type: 'SOURCE',
+        btx_component_name: 'component',
+        btx_component_plugin_name: 'plugin',
+        btx_component_path: './test/SOURCE.plugin.component',
+        btx_component_downtream_model: './test/cases_filter_state_machine/1.A.btx_model.yaml',
+      },
+      {
+        btx_component_type: 'FILTER',
+        btx_component_name: 'filter_component',
+        btx_component_plugin_name: 'filter_plugin',
+        btx_component_upstream_model: './test/cases_filter_state_machine/1.A.btx_model.yaml',
+        btx_component_downtream_model: './test/cases_filter_state_machine/1.A.btx_model.yaml',
+        btx_file_usr_callbacks: './test/cases_filter_state_machine/2.callbacks.c'
+      }
+    ]
+
+    @btx_output_validation = './test/cases_filter_state_machine/1.A.btx_log.txt'
+  end
+end
+
+class TestFilterStateMachinePushMessagesFinalize < Test::Unit::TestCase
+  include GenericTest
+  extend VariableAccessor
+  include VariableClassAccessor
+
+  def self.startup
+    @btx_components = [
+      {
+        btx_component_type: 'SOURCE',
+        btx_component_name: 'component',
+        btx_component_plugin_name: 'plugin',
+        btx_component_path: './test/SOURCE.plugin.component',
+        btx_component_downtream_model: './test/cases_filter_state_machine/1.A.btx_model.yaml',
+      },
+      {
+        btx_component_type: 'FILTER',
+        btx_component_name: 'filter_component',
+        btx_component_plugin_name: 'filter_plugin',
+        btx_component_upstream_model: './test/cases_filter_state_machine/1.A.btx_model.yaml',
+        btx_component_downtream_model: './test/cases_filter_state_machine/1.A.btx_model.yaml',
+        btx_file_usr_callbacks: './test/cases_filter_state_machine/3.callbacks.c'
+      }
+    ]
+
+    @btx_output_validation = './test/cases_filter_state_machine/1.A.btx_log.txt'
+  end
+end
