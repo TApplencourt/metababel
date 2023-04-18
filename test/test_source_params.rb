6dc96e61903bbb96e15ab04de4d685873ba58e3e
Thomas Applencourt <TApplencourt@users.noreply.github.com>
2023-04-18 18:10:15 -0500

    Add distpach test and callbacks (#36)
    * Add distpach test and callbacks
    
    * Add missing file
    
    * use calloc
    
    * Remove template

---
+++ test/test_source_params.rb
@@ -0,0 +1,19 @@
+require 'base_test'
+
+class TestSourceParamsEmpty < Test::Unit::TestCase
+  include GenericTest
+  extend VariableAccessor
+  include VariableClassAccessor
+
+  def self.startup
+    @btx_components = [
+      {
+        btx_component_type: 'SOURCE',
+        btx_component_params: './test/cases_source_params/1.btx_params_model.yaml',
+        btx_component_downtream_model: './test/cases_source_params/1.btx_model.yaml',
+        btx_file_usr_callbacks: './test/cases_source_params/1.callback.c',
+        btx_log_path: './test/cases_source_params/1.btx_log.txt',
+      }
+    ]
+  end
+end
