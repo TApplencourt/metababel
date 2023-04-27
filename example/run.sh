e0844e8791e18c06f01d0bb19475401f8aa01d7e
Thomas Applencourt <apl@anl.gov>
2023-04-27 20:07:43 +0000

    Fix one memory leak

--- example/run.sh
+++ example/run.sh
@@ -4,10 +4,10 @@
 ruby -I ../lib ../bin/metababel --downstream fake_api.yaml --component SOURCE -o btx_source
 gcc -o btx_source.so btx_source/*.c btx_source/metababel/*.c -I btx_source/ -I ../include/ $(pkg-config --cflags babeltrace2) $(pkg-config --libs babeltrace2) -fpic --shared
 
-babeltrace2 --plugin-path=. --component=source.metababel_source.btx_source
+babeltrace2 --plugin-path=. --component=source.metababel_source.btx
 
 # Sink
 ruby -I../lib ../bin/metababel --upstream fake_api.yaml --component SINK -o btx_sink
 gcc -o btx_sink.so btx_sink/*.c btx_sink/metababel/*.c -I btx_sink/ -I ../include/ $(pkg-config --cflags babeltrace2) $(pkg-config --libs babeltrace2) -fpic --shared
 
-babeltrace2 --plugin-path=. --component=source.metababel_source.btx_source  --component=sink.metababel_sink.btx_sink
+babeltrace2 --plugin-path=. --component=source.metababel_source.btx --component=sink.metababel_sink.btx
