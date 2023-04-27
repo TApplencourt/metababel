d3eea28e4249fe8d7e4bb52504de44f9e0f0a703
Thomas Applencourt <apl@anl.gov>
2023-04-27 19:50:55 +0000

    fix line

--- test/base_test.rb
+++ test/base_test.rb
@@ -48,11 +48,12 @@ end
 def get_component_compilation_command(component)
   uuid = %w[type plugin_name name].filter_map { |k| component["btx_component_#{k}".to_sym] }.join('_')
   command = <<~TEXT
-    ${CC:-cc} -o #{component[:btx_component_path]}/#{uuid}.so
-               #{component[:btx_component_path]}/*.c #{component[:btx_component_path]}/metababel/*.c
-               -I ./include -I #{component[:btx_component_path]}/
-               $(pkg-config --cflags babeltrace2) $(pkg-config --libs babeltrace2)
-               ${CFLAGS:='-Wall -Werror'} -fpic --shared
+    ${CC:-cc} -g
+              -o #{component[:btx_component_path]}/#{uuid}.so
+              #{component[:btx_component_path]}/*.c #{component[:btx_component_path]}/metababel/*.c
+              -I ./include -I #{component[:btx_component_path]}/
+              $(pkg-config --cflags babeltrace2) $(pkg-config --libs babeltrace2)
+              ${CFLAGS:='-Wall -Werror'} -fpic --shared
   TEXT
   command.split.join(' ')
 end
