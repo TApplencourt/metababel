1ee9922d6078b720d0de015ac2222d1bab7e4ab7
Thomas Applencourt <tapplencourt@anl.gov>
2023-04-28 16:32:07 +0000

    Add filter

--- example/run.sh
+++ example/run.sh
@@ -11,3 +11,9 @@ ruby -I../lib ../bin/metababel --upstream fake_api.yaml --component SINK -o btx_
 gcc -o btx_sink.so btx_sink/*.c btx_sink/metababel/*.c -I btx_sink/ -I ../include/ $(pkg-config --cflags babeltrace2) $(pkg-config --libs babeltrace2) -fpic --shared
 
 babeltrace2 --plugin-path=. --component=source.metababel_source.btx --component=sink.metababel_sink.btx
+
+#Filter
+ruby -I../lib ../bin/metababel --upstream fake_api.yaml  --downstream fake_api.yaml --component FILTER -o btx_filter
+gcc -o btx_filter.so btx_filter/*.c btx_filter/metababel/*.c -I btx_filter/ -I ../include/ $(pkg-config --cflags babeltrace2) $(pkg-config --libs babeltrace2) -fpic --shared
+
+babeltrace2 --plugin-path=. --component=source.metababel_source.btx  --component=filter.metababel_filter.btx --component=sink.metababel_sink.btx
