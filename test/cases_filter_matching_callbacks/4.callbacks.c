2177b85452a9057114ca2334099e2b8777f57da3
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2023-05-23 16:35:26 +0000

    suggestions

--- test/cases_filter_matching_callbacks/4.callbacks.c
+++ test/cases_filter_matching_callbacks/4.callbacks.c
@@ -4,7 +4,6 @@
 
 struct data_s {
   uint64_t condition_calls_count;
-  uint64_t callback_calls_count;
 };
 
 typedef struct data_s data_t;
@@ -17,7 +16,6 @@ void btx_finalize_usr_data(void *btx_handle, void *usr_data) {
   data_t *data = (data_t *)usr_data;
 
   assert(data->condition_calls_count == 4);
-  assert(data->callback_calls_count == 4);
 
   free(data);
 }
@@ -31,15 +29,12 @@ static void btx_condition(void *btx_handle, void *usr_data, bool *matched, const
 
 static void btx_callback(void *btx_handle, void *usr_data, const char* event_class_name)
 {
-  data_t *data = (data_t *)usr_data;
-  if (0 == strcmp(event_class_name, "event_1")) data->callback_calls_count += 1;
+  assert(0 == strcmp(event_class_name, "event_1"));
 }
 
 void btx_register_usr_callbacks(void *btx_handle) {
   btx_register_callbacks_initialize_usr_data(btx_handle, &btx_initialize_usr_data);
   btx_register_callbacks_finalize_usr_data(btx_handle, &btx_finalize_usr_data);
 
-  // We can use the same condition and callback function because scA and scB do not have common fields
-  // Then the function signature is the same in both cases.
   btx_register_matching_callback_scA(btx_handle, &btx_condition, &btx_callback);
 }
