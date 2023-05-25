5f56c443285477e64bc8dcb2e1a17794b966bf8d
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2023-05-25 16:47:36 +0000

    bt2 simple filter

---
+++ example_bt2/btx_filter/callbacks.c
@@ -0,0 +1,61 @@
+#include <metababel/metababel.h>
+#include <stdbool.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+
+struct data_s {
+  const char *names_value;
+};
+
+typedef struct data_s data_t;
+
+void btx_initialize_usr_data(void *btx_handle, void **usr_data) {
+  *usr_data = calloc(1, sizeof(data_t));
+}
+
+void btx_read_params(void *btx_handle, void *usr_data, btx_params_t *usr_params) {
+  ((data_t *)usr_data)->names_value = usr_params->names;
+}
+
+void btx_finalize_usr_data(void *btx_handle, void *usr_data) {
+  data_t *data = (data_t *)usr_data;
+  free(data);
+}
+
+static void btx_filter_condition(void *btx_handle, void *usr_data, const char* event_class_name, bool *matched)
+{
+  data_t *data = (data_t *)usr_data;
+
+  int str_size = strlen(data->names_value);
+  char str[str_size];
+
+  strcpy(str, data->names_value);
+
+	char *ptr = strtok(str, ",");
+	bool ocurrence = false;
+
+  while(ptr != NULL && !ocurrence)
+	{
+    ocurrence = (strcmp(ptr,event_class_name) == 0);
+		ptr = strtok(NULL, ",");
+	}
+
+  // If the event_class_name is found in 'data->names_value', the associated callback is called,
+  // this resulting in the event being discared.
+  *matched = ocurrence;
+}
+
+static void btx_filter_callback(void *btx_handle, void *usr_data, const char* event_class_name)
+{
+
+}
+
+void btx_register_usr_callbacks(void *btx_handle) {
+  btx_register_callbacks_initialize_usr_data(btx_handle, &btx_initialize_usr_data);
+  btx_register_callbacks_finalize_usr_data(btx_handle, &btx_finalize_usr_data);
+
+  btx_register_callbacks_read_params(btx_handle, &btx_read_params);
+  
+  btx_register_matching_callback_sc(btx_handle, &btx_filter_condition, &btx_filter_callback);
+}
