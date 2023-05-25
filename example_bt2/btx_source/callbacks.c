c3d6b0b8ef11cd7d757adb122d8192172f8ea3b2
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2023-05-25 16:53:09 +0000

    update

--- example_bt2/btx_source/callbacks.c
+++ example_bt2/btx_source/callbacks.c
@@ -2,15 +2,15 @@
 #include <stdbool.h>
 
 void btx_push_usr_messages(void *btx_handle, void *usr_data, btx_source_status_t *status) {
-    btx_push_message_sched_switch(btx_handle);
-    btx_push_message_rcu_utilization(btx_handle);
-    btx_push_message_kmem_kfree(btx_handle);
-    btx_push_message_event_1(btx_handle);
-    btx_push_message_event_2(btx_handle);
-    btx_push_message_event_3(btx_handle);
-    *status = BTX_SOURCE_END;
+  btx_push_message_sched_switch(btx_handle);
+  btx_push_message_rcu_utilization(btx_handle);
+  btx_push_message_kmem_kfree(btx_handle);
+  btx_push_message_event_1(btx_handle);
+  btx_push_message_event_2(btx_handle);
+  btx_push_message_event_3(btx_handle);
+  *status = BTX_SOURCE_END;
 }
 
 void btx_register_usr_callbacks(void *btx_handle) {
-btx_register_callbacks_push_usr_messages(btx_handle, &btx_push_usr_messages);
+  btx_register_callbacks_push_usr_messages(btx_handle, &btx_push_usr_messages);
 }
