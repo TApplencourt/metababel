ebe21abdcc93e3b80e6439e7f90705671acf3ba8
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2023-05-31 23:05:33 +0000

    timestamp and result

--- example/btx_filter_hip/callbacks.c
+++ example/btx_filter_hip/callbacks.c
@@ -6,6 +6,7 @@
 struct data_s {
   uint64_t entry_count;
   uint64_t exit_count;
+  uint64_t hipResult;
 };
 
 typedef struct data_s data_t;
@@ -19,35 +20,47 @@ void btx_finalize_usr_data(void *btx_handle, void *usr_data) {
 
   printf("entry_count: %d\n", data->entry_count);
   printf("exit_count: %d\n", data->exit_count);
+  printf("hipResult: %d\n", data->hipResult);
 
   free(data);
 }
 
-static void btx_entry_matcher(void *btx_handle, void *usr_data, const char* event_class_name, bool *matched, int64_t vpid, uint64_t vtid)
+static void btx_hip_entry_matcher(void *btx_handle, void *usr_data, const char* event_class_name, 
+                                  bool *matched, uint64_t timestamp, int64_t vpid, uint64_t vtid)
 {
   data_t *data = (data_t *)usr_data;
   *matched = strstr(event_class_name, "_entry") != NULL;
 }
 
-static void btx_entry_callback(void *btx_handle, void *usr_data, const char* event_class_name, int64_t vpid, uint64_t vtid) {
+static void btx_hip_entry_callback(void *btx_handle, void *usr_data, const char* event_class_name, 
+                                   uint64_t timestamp, int64_t vpid, uint64_t vtid) 
+{
   data_t *data = (data_t *)usr_data;
   data->entry_count += 1;
 }
 
-static void btx_exit_matcher(void *btx_handle, void *usr_data, const char* event_class_name, bool *matched, int64_t vpid, uint64_t vtid)
+static void btx_hip_exit_matcher(void *btx_handle, void *usr_data, const char* event_class_name, bool *matched, 
+                                 uint64_t timestamp, int64_t vpid, uint64_t vtid)
 {
   data_t *data = (data_t *)usr_data;
   *matched = strstr(event_class_name, "_exit") != NULL;
 }
 
-static void btx_exit_callback(void *btx_handle, void *usr_data, const char* event_class_name, int64_t vpid, uint64_t vtid) {
+static void btx_hip_exit_callback(void *btx_handle, void *usr_data, const char* event_class_name, 
+                                  uint64_t timestamp, int64_t vpid, uint64_t vtid) 
+{
   data_t *data = (data_t *)usr_data;
   data->exit_count += 1;
+
+  int64_t hipResult;
+  bool succeed = false;
+  btx_event_payload_field_integer_signed_get_value(btx_handle,"hipResult",&hipResult,&succeed);
+  data->hipResult = hipResult;
 }
 
 void btx_register_usr_callbacks(void *btx_handle) {
   btx_register_callbacks_initialize_usr_data(btx_handle, &btx_initialize_usr_data);
   btx_register_callbacks_finalize_usr_data(btx_handle, &btx_finalize_usr_data);
-  btx_register_matching_callback_hip(btx_handle, &btx_entry_matcher, &btx_entry_callback);
-  btx_register_matching_callback_hip(btx_handle, &btx_exit_matcher, &btx_exit_callback);
+  btx_register_matching_callback_hip(btx_handle, &btx_hip_entry_matcher, &btx_hip_entry_callback);
+  btx_register_matching_callback_hip(btx_handle, &btx_hip_exit_matcher, &btx_hip_exit_callback);
 }
--- example/run.sh
+++ example/run.sh
@@ -1,27 +1,37 @@
 #!/bin/bash -xe
 
-# Source
-ruby -I ../lib ../bin/metababel --downstream fake_api.yaml --component SOURCE -o btx_source
-gcc -g -o btx_source.so btx_source/*.c btx_source/metababel/*.c -I btx_source/ -I ../include/ $(pkg-config --cflags babeltrace2) $(pkg-config --libs babeltrace2) -fpic --shared
-babeltrace2 --plugin-path=. --component=source.metababel_source.btx
+# # Source
+# ruby -I ../lib ../bin/metababel --downstream fake_api.yaml --component SOURCE -o btx_source
+# gcc -g -o btx_source.so btx_source/*.c btx_source/metababel/*.c -I btx_source/ -I ../include/ $(pkg-config --cflags babeltrace2) $(pkg-config --libs babeltrace2) -fpic --shared
+# babeltrace2 --plugin-path=. --component=source.metababel_source.btx
 
-# Sink
-ruby -I../lib ../bin/metababel --upstream fake_api.yaml --component SINK -o btx_sink
-gcc -g -o btx_sink.so btx_sink/*.c btx_sink/metababel/*.c -I btx_sink/ -I ../include/ $(pkg-config --cflags babeltrace2) $(pkg-config --libs babeltrace2) -fpic --shared
-babeltrace2 --plugin-path=. --component=source.metababel_source.btx --component=sink.metababel_sink.btx
+# # Sink
+# ruby -I../lib ../bin/metababel --upstream fake_api.yaml --component SINK -o btx_sink
+# gcc -g -o btx_sink.so btx_sink/*.c btx_sink/metababel/*.c -I btx_sink/ -I ../include/ $(pkg-config --cflags babeltrace2) $(pkg-config --libs babeltrace2) -fpic --shared
+# babeltrace2 --plugin-path=. --component=source.metababel_source.btx --component=sink.metababel_sink.btx
 
-#Filter
-ruby -I../lib ../bin/metababel --upstream fake_api.yaml  --downstream fake_api.yaml --component FILTER -o btx_filter
-gcc -g -o btx_filter.so btx_filter/*.c btx_filter/metababel/*.c -I btx_filter/ -I ../include/ $(pkg-config --cflags babeltrace2) $(pkg-config --libs babeltrace2) -fpic --shared
-babeltrace2 --plugin-path=. --component=source.metababel_source.btx  --component=filter.metababel_filter.btx --component=sink.metababel_sink.btx
+# #Filter
+# ruby -I../lib ../bin/metababel --upstream fake_api.yaml  --downstream fake_api.yaml --component FILTER -o btx_filter
+# gcc -g -o btx_filter.so btx_filter/*.c btx_filter/metababel/*.c -I btx_filter/ -I ../include/ $(pkg-config --cflags babeltrace2) $(pkg-config --libs babeltrace2) -fpic --shared
+# babeltrace2 --plugin-path=. --component=source.metababel_source.btx  --component=filter.metababel_filter.btx --component=sink.metababel_sink.btx
 
+
+# # Source timestamp
+# ruby -I ../lib ../bin/metababel --downstream dust.yaml --component SOURCE -o btx_dust -p dust --params dust_params.yaml
+# gcc -g -o btx_source.so btx_dust/*.c btx_dust/metababel/*.c -I btx_dust/ -I ../include/ $(pkg-config --cflags babeltrace2) $(pkg-config --libs babeltrace2) -fpic --shared
+# babeltrace2 --plugin-path=. --component=source.dust.btx  --params='path="./btx_dust/dust.txt"'
+
+# # Filter timestamp
+# ruby -I../lib ../bin/metababel --upstream dust.yaml  --downstream dust.yaml --component FILTER -p distill -c theone -o btx_filter_distill --params dust_params.yaml
+# gcc -g -o btx_filter_distill.so btx_filter_distill/*.c btx_filter_distill/metababel/*.c -I btx_filter_distill/ -I ../include/ $(pkg-config --cflags babeltrace2) $(pkg-config --libs babeltrace2) -fpic --shared
+# babeltrace2 --plugin-path=. --component=source.dust.btx  --params='path="./btx_dust/dust.txt"' --component=filter.distill.theone --params='names="sched_switch,rcu_utilization,kmem_kfree"'
 
 # Source timestamp
-ruby -I ../lib ../bin/metababel --downstream dust.yaml --component SOURCE -o btx_dust -p dust --params dust_params.yaml
-gcc -g -o btx_source.so btx_dust/*.c btx_dust/metababel/*.c -I btx_dust/ -I ../include/ $(pkg-config --cflags babeltrace2) $(pkg-config --libs babeltrace2) -fpic --shared
-babeltrace2 --plugin-path=. --component=source.dust.btx  --params='path="./btx_dust/dust.txt"'
+ruby -I ../lib ../bin/metababel --downstream hip_model.yaml --component SOURCE -o btx_source_hip -p hip_sp
+gcc -g -o btx_source_hip.so btx_source_hip/*.c btx_source_hip/metababel/*.c -I btx_source_hip/ -I ../include/ $(pkg-config --cflags babeltrace2) $(pkg-config --libs babeltrace2) -fpic --shared
+babeltrace2 --plugin-path=. --component=source.hip_sp.btx
 
 # Filter timestamp
-ruby -I../lib ../bin/metababel --upstream dust.yaml  --downstream dust.yaml --component FILTER -p distill -c theone -o btx_filter_distill --params dust_params.yaml
-gcc -g -o btx_filter_distill.so btx_filter_distill/*.c btx_filter_distill/metababel/*.c -I btx_filter_distill/ -I ../include/ $(pkg-config --cflags babeltrace2) $(pkg-config --libs babeltrace2) -fpic --shared
-babeltrace2 --plugin-path=. --component=source.dust.btx  --params='path="./btx_dust/dust.txt"' --component=filter.distill.theone --params='names="sched_switch,rcu_utilization,kmem_kfree"'
+ruby -I../lib ../bin/metababel --upstream hip_model.yaml --downstream interval_model.yaml --component FILTER -p hip_fp -o btx_filter_hip
+gcc -g -o btx_filter_hip.so btx_filter_hip/*.c btx_filter_hip/metababel/*.c -I btx_filter_hip/ -I ../include/ $(pkg-config --cflags babeltrace2) $(pkg-config --libs babeltrace2) -fpic --shared
+babeltrace2 --plugin-path=. --component=source.hip_sp.btx --component=filter.hip_fp.btx
