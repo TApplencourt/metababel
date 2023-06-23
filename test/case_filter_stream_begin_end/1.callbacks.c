8d542054b5e2d9d74e330e138b22d1f9561c1a68
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2023-06-23 22:38:51 +0000

    update

--- test/case_filter_stream_begin_end/1.callbacks.c
+++ test/case_filter_stream_begin_end/1.callbacks.c
@@ -1,2 +1,9 @@
 #include <metababel/metababel.h>
-void btx_register_usr_callbacks(void *btx_handle) {}
+
+void btx_usr_callback_event_1(void *btx_handle,void *usr_data) {
+    btx_push_message_event_1(btx_handle);
+}
+
+void btx_register_usr_callbacks(void *btx_handle) {
+  btx_register_callbacks_event_1(btx_handle,&btx_usr_callback_event_1);
+}
