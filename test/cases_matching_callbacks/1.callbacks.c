6cc61aa9298b7e4fb3aebc11ac3ad8ec60c095fd
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2023-07-13 21:15:57 +0000

    update

--- test/cases_matching_callbacks/1.callbacks.c
+++ test/cases_matching_callbacks/1.callbacks.c
@@ -35,5 +35,5 @@ void btx_register_usr_callbacks(void *btx_handle) {
   btx_register_callbacks_initialize_usr_data(btx_handle, &btx_initialize_usr_data);
   btx_register_callbacks_finalize_usr_data(btx_handle, &btx_finalize_usr_data);
   btx_register_callbacks_event_1(btx_handle, &btx_event_callback);
-  btx_register_matching_callbacks_usr(btx_handle, &btx_matching_callback);
+  btx_register_matching_callbacks_foo(btx_handle, &btx_matching_callback);
 }
