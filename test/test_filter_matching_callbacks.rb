00ba1a3396892205233e2647ffaf19053f551173
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2023-07-13 21:24:47 +0000

    fix

--- test/test_filter_matching_callbacks.rb
+++ test/test_filter_matching_callbacks.rb
@@ -1,31 +1,31 @@
 require 'base_test'
 
-# class TestFilterMatchingCallbacksCalled < Test::Unit::TestCase
-#   # Validate the proper calling of both regular and matching callbacks.
-#   # Regular callbacks dispatch event_1 while matching callbacks dispatch
-#   # event_2. 
+class TestFilterMatchingCallbacksCalled < Test::Unit::TestCase
+  # Validate the proper calling of both regular and matching callbacks.
+  # Regular callbacks dispatch event_1 while matching callbacks dispatch
+  # event_2. 
 
-#   include GenericTest
-#   extend VariableAccessor
-#   include VariableClassAccessor
+  include GenericTest
+  extend VariableAccessor
+  include VariableClassAccessor
 
-#   def self.startup
-#     @btx_components = [
-#       {
-#         btx_component_type: 'SOURCE',
-#         btx_component_downstream_model: './test/cases_matching_callbacks/1.btx_model.yaml',
-#         btx_log_path: './test/cases_matching_callbacks/1.btx_log.txt'
-#       },
-#       {
-#         btx_component_type: 'FILTER',
-#         btx_component_upstream_model: './test/cases_matching_callbacks/1.btx_model.yaml',
-#         btx_component_downstream_model: './test/cases_matching_callbacks/1.btx_model.yaml',
-#         btx_component_callbacks: './test/cases_matching_callbacks/1.btx_callbacks.yaml',
-#         btx_file_usr_callbacks: './test/cases_matching_callbacks/1.callbacks.c'
-#       }
-#     ]
-#   end
-# end
+  def self.startup
+    @btx_components = [
+      {
+        btx_component_type: 'SOURCE',
+        btx_component_downstream_model: './test/cases_matching_callbacks/1.btx_model.yaml',
+        btx_log_path: './test/cases_matching_callbacks/1.btx_log.txt'
+      },
+      {
+        btx_component_type: 'FILTER',
+        btx_component_upstream_model: './test/cases_matching_callbacks/1.btx_model.yaml',
+        btx_component_downstream_model: './test/cases_matching_callbacks/1.btx_model.yaml',
+        btx_component_callbacks: './test/cases_matching_callbacks/1.btx_callbacks.yaml',
+        btx_file_usr_callbacks: './test/cases_matching_callbacks/1.callbacks.c'
+      }
+    ]
+  end
+end
 
 class TestFilterMatchingCallbacksCalledWithTimeStamp < Test::Unit::TestCase
   # Validate the _timestamp is passed properly in matching callbacks.
