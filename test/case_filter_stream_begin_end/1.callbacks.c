99457d04d2caab39e4daa06e7d397275a16d8f20
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2023-08-08 18:29:35 +0000

    no missing messages validations

--- test/case_filter_stream_begin_end/1.callbacks.c
+++
@@ -1,9 +0,0 @@
-#include <metababel/metababel.h>
-
-void btx_usr_callback_event_1(void *btx_handle,void *usr_data) {
-    btx_push_message_event_1(btx_handle);
-}
-
-void btx_register_usr_callbacks(void *btx_handle) {
-  btx_register_callbacks_event_1(btx_handle,&btx_usr_callback_event_1);
-}
