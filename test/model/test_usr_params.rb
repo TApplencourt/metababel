88cf08026a3002843ec7801922d9b465e3399e53
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2023-08-14 20:19:04 +0000

    add/remove spaces

--- test/model/test_usr_params.rb
+++ test/model/test_usr_params.rb
@@ -13,7 +13,6 @@ class TestUserParams < Test::Unit::TestCase
         btx_component_params_model: './test/model/cases_usr_params/1.btx_params.yaml',
         btx_component_params: 'param_1=\"\",param_2=false,param_3=1',
         btx_file_usr_callbacks: './test/model/cases_usr_params/1.callbacks.c',
-
       },
       {
         btx_component_type: 'FILTER',
