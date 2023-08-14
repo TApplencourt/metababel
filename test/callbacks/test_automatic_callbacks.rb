c261390fa1f2266e31e79ba368fd0966b5169567
Aurelio Vivas <aa.vivas@uniandes.edu.co>
2023-08-14 17:38:15 -0500

    Add test for calling two callbacks for the same event. (#90)
    * update to send user params to components
    
    * update model tests
    
    * add/remove spaces
    
    * add/remove spaces
    
    * test calling two callbacks for one incoming event

--- test/callbacks/test_automatic_callbacks.rb
+++ test/callbacks/test_automatic_callbacks.rb
@@ -23,3 +23,27 @@ class TestCallAutomaticCallbackWithTimestamp < Test::Unit::TestCase
     @btx_output_validation = './test/callbacks/cases_automatic_callbacks/1.btx_log.txt'
   end
 end
+
+class TestCallTwoCallbacksForOneEvent < Test::Unit::TestCase
+  include GenericTest
+  extend VariableAccessor
+  include VariableClassAccessor
+
+  def self.startup
+    @btx_components = [
+      {
+        btx_component_type: 'SOURCE',
+        btx_component_downstream_model: './test/callbacks/cases_automatic_callbacks/2.btx_model.yaml',
+        btx_log_path: './test/callbacks/cases_automatic_callbacks/2.btx_log.in'
+      },
+      {
+        btx_component_type: 'FILTER',
+        btx_component_upstream_model: './test/callbacks/cases_automatic_callbacks/2.btx_model.yaml',
+        btx_component_downstream_model: './test/callbacks/cases_automatic_callbacks/2.btx_model.yaml',
+        btx_file_usr_callbacks: './test/callbacks/cases_automatic_callbacks/2.callbacks.c'
+      }
+    ]
+
+    @btx_output_validation = './test/callbacks/cases_automatic_callbacks/2.btx_log.out'
+  end
+end
