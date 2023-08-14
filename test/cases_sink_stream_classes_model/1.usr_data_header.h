fafbcd23d766392d44b4163b7f5b6e11ba8d174e
Aurelio Vivas <aa.vivas@uniandes.edu.co>
2023-08-14 15:30:20 -0500

    Update model tests (#89)
    * update to send user params to components
    
    * update model tests
    
    * add/remove spaces
    
    * add/remove spaces

--- test/cases_sink_stream_classes_model/1.usr_data_header.h
+++
@@ -1,7 +0,0 @@
-#include <stdint.h>
-#include <babeltrace2/babeltrace.h>
-
-typedef bt_bool usr_bool_t;
-typedef char * usr_string_t;
-typedef uint64_t usr_uint64_t;
-typedef int64_t usr_int64_t;
---
+++ test/model/cases_usr_header/1.usr_header.h
@@ -0,0 +1,1 @@
+typedef char * usr_string_t;
