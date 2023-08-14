3b53dbca0881522e95dafb3b977baa1306599a16
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2023-08-14 15:25:13 +0000

    add space

--- test/callbacks/cases_automatic_callbacks/1.callbacks.c
+++ test/callbacks/cases_automatic_callbacks/1.callbacks.c
@@ -1,7 +1,7 @@
 #include <metababel/metababel.h>
 
 static void event_callback(void *btx_handle, void *usr_data, int64_t timestamp) {
-  btx_push_message_event(btx_handle,timestamp);
+  btx_push_message_event(btx_handle, timestamp);
 }
 
 void btx_register_usr_callbacks(void *btx_handle) {
