db58c61136efaa2a7850f53f6642de60a0059cdf
Aurelio Vivas <aurelio.vivas@correounivalle.edu.co>
2023-09-08 20:43:35 +0000

    Fix {}.match? for default_clock_class: {}

--- lib/metababel/bt2_matching_utils.rb
+++ lib/metababel/bt2_matching_utils.rb
@@ -1,21 +1,24 @@
-module Babeltrace2Gen
-  class Hash
-    # Special case for ':default_clock_class: {}''
+module HashRefinements
+  refine Hash do
+    # Special case for emty hash {} in ':default_clock_class: {}'.
     def match?(obj)
       self == obj ? [] : nil
     end
   end
+end
 
+module Babeltrace2Gen
   module BTMatch
     def match?(obj)
       match_attrs = self.class.class_variable_get(:@@bt_match_attrs)
       match = attrs_match?(match_attrs, self, obj).flatten
-      # forward nil value if encountered.
+      # forward nil value if encountered; ortherwise, trasnform returned args if not transformed yet.
       match.include?(nil) ? nil : match.map { |m| m.respond_to?(:get_arg) ? m.get_arg : m }
     end
   end
 
   module BTMatchUtils
+    using HashRefinements
     # Required to properly parse the result of match?
     # when applied on native data types such as String.
     def normalize(obj)
@@ -42,7 +45,7 @@ module Babeltrace2Gen
       matches = objs.map { |obj| obj.match?(match_obj) ? obj : nil }.compact
       raise "Match expression '#{match_obj.name}' must match only one member, '#{ matches.length }' matched." unless matches.length < 2
       
-      # If not argument matched, then nil. 
+      # If not argument matched, then nil (no matched argument).
       matches.empty? ? nil : matches
       end.flatten(1)
 
