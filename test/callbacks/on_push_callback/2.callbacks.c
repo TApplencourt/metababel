fc9890a633041a877497269699735b83e49a7cc5
Thomas Applencourt <tapplencourt@anl.gov>
2023-10-12 20:37:27 +0000

    Add new tests and improve doc

--- test/callbacks/on_push_callback/2.callbacks.c
+++ test/callbacks/on_push_callback/2.callbacks.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <babeltrace2/babeltrace.h>
 #include <metababel/metababel.h>
 
@@ -5,30 +6,23 @@ void btx_initialize_usr_data(void *btx_handle, void **usr_data) {
   *usr_data = calloc(1, sizeof(int));
 }
 
-void btx_finalize_usr_data(void *btx_handle, void *usr_data) { free(usr_data); }
-
-static void event_callback(void *btx_handle, void *usr_data,
-                           int64_t timestamp) {
-  btx_push_message_event(btx_handle, timestamp);
+void btx_finalize_usr_data(void *btx_handle, void *usr_data) {
+  // 2 begin/end * 2 (one for source, one for filter)
+  // + 2 message sent by the source and forwarded by the filter
+  // = 6 messages totals
+  assert(*(int *)(usr_data) == 6);
+  free(usr_data);
 }
 
 static void on_push_callback(void *btx_handle, void *usr_data,
                              const bt_message *message) {
-  // Just forward the first message
-  if (bt_message_get_type(message) == BT_MESSAGE_TYPE_EVENT)
-    *(int *)(usr_data) += 1;
-
-  if (*(int *)(usr_data) == 1 ||
-      bt_message_get_type(message) != BT_MESSAGE_TYPE_EVENT)
-    btx_push_message(btx_handle, message);
-  else
-    bt_message_put_ref(message);
+  *(int *)(usr_data) += 1;
+  btx_push_message(btx_handle, message);
 }
 
 void btx_register_usr_callbacks(void *btx_handle) {
   btx_register_callbacks_initialize_component(btx_handle,
                                               &btx_initialize_usr_data);
   btx_register_callbacks_finalize_component(btx_handle, &btx_finalize_usr_data);
-  btx_register_callbacks_event(btx_handle, &event_callback);
   btx_register_on_push_callback(btx_handle, &on_push_callback);
 }
--- test/callbacks/test_on_push_callbacks.rb
+++ test/callbacks/test_on_push_callbacks.rb
@@ -23,3 +23,25 @@ class TestCallOnPushCallbackWithTimestamp < Test::Unit::TestCase
     @btx_output_validation = './test/callbacks/on_push_callback/1.btx_log_out.txt'
   end
 end
+
+class TestCallOnPushCallbackCount < Test::Unit::TestCase
+  include GenericTest
+  extend VariableAccessor
+  include VariableClassAccessor
+
+  def self.startup
+    @btx_components = [
+      {
+        btx_component_type: 'SOURCE',
+        btx_component_downstream_model: './test/callbacks/on_push_callback/1.btx_model.yaml',
+        btx_log_path: './test/callbacks/on_push_callback/1.btx_log.txt',
+      },
+      {
+        btx_component_type: 'FILTER',
+        btx_component_upstream_model: './test/callbacks/on_push_callback/1.btx_model.yaml',
+        btx_component_downstream_model: './test/callbacks/on_push_callback/1.btx_model.yaml',
+        btx_file_usr_callbacks: './test/callbacks/on_push_callback/2.callbacks.c',
+      },
+    ]
+  end
+end
