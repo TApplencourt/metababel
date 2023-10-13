52edbb6ff1b543c16358ff26bc6b977058154a19
Thomas Applencourt <tapplencourt@anl.gov>
2023-10-13 16:07:26 +0000

    Fix path

--- test/callbacks/test_on_push_callbacks.rb
+++ test/callbacks/test_on_push_callbacks.rb
@@ -9,18 +9,18 @@ class TestCallOnPushCallbackWithTimestamp < Test::Unit::TestCase
     @btx_components = [
       {
         btx_component_type: 'SOURCE',
-        btx_component_downstream_model: './test/callbacks/on_push_callback/1.btx_model.yaml',
-        btx_log_path: './test/callbacks/on_push_callback/1.btx_log.txt',
+        btx_component_downstream_model: './test/callbacks/case_on_push_callback/1.btx_model.yaml',
+        btx_log_path: './test/callbacks/case_on_push_callback/1.btx_log.txt',
       },
       {
         btx_component_type: 'FILTER',
-        btx_component_upstream_model: './test/callbacks/on_push_callback/1.btx_model.yaml',
-        btx_component_downstream_model: './test/callbacks/on_push_callback/1.btx_model.yaml',
-        btx_file_usr_callbacks: './test/callbacks/on_push_callback/1.callbacks.c',
+        btx_component_upstream_model: './test/callbacks/case_on_push_callback/1.btx_model.yaml',
+        btx_component_downstream_model: './test/callbacks/case_on_push_callback/1.btx_model.yaml',
+        btx_file_usr_callbacks: './test/callbacks/case_on_push_callback/1.callbacks.c',
       },
     ]
 
-    @btx_output_validation = './test/callbacks/on_push_callback/1.btx_log_out.txt'
+    @btx_output_validation = './test/callbacks/case_on_push_callback/1.btx_log_out.txt'
   end
 end
 
@@ -33,14 +33,14 @@ class TestCallOnPushCallbackCount < Test::Unit::TestCase
     @btx_components = [
       {
         btx_component_type: 'SOURCE',
-        btx_component_downstream_model: './test/callbacks/on_push_callback/1.btx_model.yaml',
-        btx_log_path: './test/callbacks/on_push_callback/1.btx_log.txt',
+        btx_component_downstream_model: './test/callbacks/case_on_push_callback/1.btx_model.yaml',
+        btx_log_path: './test/callbacks/case_on_push_callback/1.btx_log.txt',
       },
       {
         btx_component_type: 'FILTER',
-        btx_component_upstream_model: './test/callbacks/on_push_callback/1.btx_model.yaml',
-        btx_component_downstream_model: './test/callbacks/on_push_callback/1.btx_model.yaml',
-        btx_file_usr_callbacks: './test/callbacks/on_push_callback/2.callbacks.c',
+        btx_component_upstream_model: './test/callbacks/case_on_push_callback/1.btx_model.yaml',
+        btx_component_downstream_model: './test/callbacks/case_on_push_callback/1.btx_model.yaml',
+        btx_file_usr_callbacks: './test/callbacks/case_on_push_callback/2.callbacks.c',
       },
     ]
   end
