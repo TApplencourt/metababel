eba282d4fc1d2c93fb5c0bb842131f75c2eae22e
Thomas Applencourt <apl@anl.gov>
2024-01-23 23:50:14 +0000

    update example

--- example/btx_source/callbacks.c
+++ example/btx_source/callbacks.c
@@ -1,5 +1,8 @@
 #include <metababel/metababel.h>
 
+void btx_init_proc(void *btx_handle, void *usr_data) {
+  btx_downstream_set_environment_dummy(btx_handle, "roger");
+}
 
 void btx_push_usr_messages(void *btx_handle, void *usr_data, btx_source_status_t *status) {
   int vpid = 1;
@@ -7,11 +10,12 @@ void btx_push_usr_messages(void *btx_handle, void *usr_data, btx_source_status_t
   uint64_t num_entries= 0;
   uint64_t platforms= 1;
   uint64_t num_platform = 2;
-  btx_downstream_set_environment_dummy(btx_handle, "roger");
+  btx_push_message_GetPlatformIDs(btx_handle, vpid, vtid, num_entries, platforms, num_platform);
   *status = BTX_SOURCE_END;
 }
 
 void btx_register_usr_callbacks(void *btx_handle) {
+  btx_register_callbacks_initialize_processing(btx_handle, btx_init_proc);
   btx_register_callbacks_push_usr_messages(btx_handle, &btx_push_usr_messages);
 }
 
