8f4a69d28b1a28b641e47e3e3a0cc6a5fcad6f6a
Thomas Applencourt <apl@anl.gov>
2024-03-27 19:27:13 +0000

    Add test

---
+++ test/shared/test_stripper.rb
@@ -0,0 +1,32 @@
+require 'base_test'
+
+class TestStripper < Test::Unit::TestCase
+  include GenericTest
+  extend VariableAccessor
+  include VariableClassAccessor
+
+  def self.startup
+    @btx_components = [
+      {
+        btx_component_type: 'SOURCE',
+        btx_component_downstream_model: './test/shared/cases_stripper/1.btx_model.yaml',
+        btx_log_path: './test/shared/cases_stripper/1.btx_log.in',
+      },
+      {
+        btx_component_type: 'FILTER',
+        btx_file_usr_callbacks: './shared/stripper.cpp',
+        btx_component_downstream_model: './test/shared/cases_stripper/1.btx_model.yaml',
+        btx_component_upstream_model: './test/shared/cases_stripper/1.btx_model.yaml',
+        btx_component_enable_callbacks: 'on_downstream',
+      },
+      {
+        btx_component_type: 'SINK',
+        btx_component_plugin_name: 'text',
+        btx_component_name: 'details',
+        btx_compile: false,
+      }
+    ]
+
+    @btx_output_validation = './test/shared/cases_stripper/1.btx_log.out'
+  end
+end
