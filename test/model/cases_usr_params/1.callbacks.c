3cf681b51346fe205bee33daf9f89e261c842d2e
Thomas Applencourt <apl@anl.gov>
2024-04-22 22:30:10 +0000

    better test

--- test/model/cases_usr_params/1.callbacks.c
+++ test/model/cases_usr_params/1.callbacks.c
@@ -1,11 +1,11 @@
 #include <metababel/metababel.h>
 #include <assert.h>
 
-void btx_read_params(void *usr_data, btx_param_0_t *usr_params) {
+void btx_read_params(void *usr_data, btx_params_t *usr_params) {
   assert(strcmp(usr_params->param_1,"") == 0);
   assert(usr_params->param_2 == BT_FALSE);
   assert(usr_params->param_3 == 1);
-  assert(usr_params->param_4.param_4_1 == 12);
+  assert(usr_params->params_4.param_1 == 12);
 }
 
 void btx_register_usr_callbacks(void *btx_handle) {
