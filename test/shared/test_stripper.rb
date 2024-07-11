6a4396b0757f9899be67d2519f4adcf2163bc61c
Thomas Applencourt <apl@anl.gov>
2024-07-11 18:52:11 +0000

    better test

--- test/shared/test_stripper.rb
+++ test/shared/test_stripper.rb
@@ -40,14 +40,14 @@ class TestStripperParams < Test::Unit::TestCase
       {
         btx_component_type: 'SOURCE',
         btx_component_downstream_model: './test/shared/cases_stripper/1.btx_model.yaml',
-        btx_log_path: './test/shared/cases_stripper/1.btx_log.in',
+        btx_log_path: './test/shared/cases_stripper/2.btx_log.in',
       },
       {
         btx_component_type: 'FILTER',
         btx_file_usr_callbacks: './shared/stripper.cpp',
         btx_component_enable_callbacks: 'on_downstream',
         btx_component_params_model: './shared/stripper_params.yaml',
-        btx_component_params: 'filter_prefix=event2',
+        btx_component_params: 'filter_prefix=event1',
       },
       {
         btx_component_type: 'SINK',
