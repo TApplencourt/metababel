cfe1ee8863f03fd757ffee51c533232a79d145d3
Thomas Applencourt <tapplencourt@anl.gov>
2025-03-19 10:49:06 -0500

    Update run.sh

--- example/run.sh
+++ example/run.sh
@@ -19,17 +19,17 @@ babeltrace2 --plugin-path=. --component=source.metababel_source.btx --component=
 #Filter
 ruby -I../lib ../bin/metababel --component-type FILTER \
                                --upstream fake_api.yaml \
-			                         --downstream fake_api.yaml \
-			                         -o btx_filter
+			       --downstream fake_api.yaml \
+			       -o btx_filter
 gcc -g -o btx_filter.so btx_filter/*.c btx_filter/metababel/*.c -I btx_filter/ -I ../include/ ${CFLAGS}
 babeltrace2 --plugin-path=. --component=source.metababel_source.btx  --component=filter.metababel_filter.btx --component=sink.metababel_sink.btx
 
 # Source timestamp
 ruby -I ../lib ../bin/metababel  --component-type SOURCE \
                                  --downstream dust.yaml \
-				                         --component-type SOURCE \
-				                         -p dust \
-				                         --params dust_params.yaml \
-				                         -o btx_dust
+				 --component-type SOURCE \
+				 -p dust \
+				 --params dust_params.yaml \
+			         -o btx_dust
 gcc -g -o btx_source.so btx_dust/*.c btx_dust/metababel/*.c -I btx_dust/ -I ../include/ ${CFLAGS}
 babeltrace2 --plugin-path=. --component=source.dust.btx  --params='path="./btx_dust/dust.txt"'
